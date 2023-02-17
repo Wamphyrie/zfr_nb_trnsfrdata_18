66f9a36eda25e7df9e36d50ce97fa2d995d5738a
Wamphyrie <farooqsbox@yahoo.com>
2023-02-16 23:19:59 -0500

    Project not working... The first screen not loading

--- webapp/controller/Object.controller.js
+++ webapp/controller/Object.controller.js
@@ -25,6 +25,7 @@ sap.ui.define([
 			 * @public
 			 */
 			onInit : function () {
+				
 				// Model used to manipulate control states. The chosen values make sure,
 				// detail page is busy indication immediately so there is no break in
 				// between the busy indication for loading the view's meta data
